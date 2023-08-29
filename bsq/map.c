061d897f0d6810512d491b7dc8e11194c8703a70
jadamin <jadaminpro@gmail.com>
2023-08-29 11:38:18 +0200

    changement sur map.c

--- bsq/map.c
+++ bsq/map.c
@@ -10,8 +10,7 @@ char **read_map(int fd, int *num_lines, char *empty_char, char *obstacle_char, c
     char num_lines_buf[16];
     int i = 0;
     char c;
-    
-    // Read the number of lines
+
     while (read(fd, &c, 1) > 0 && c != '\n') {
         num_lines_buf[i] = c;
         i++;
@@ -19,22 +18,21 @@ char **read_map(int fd, int *num_lines, char *empty_char, char *obstacle_char, c
     num_lines_buf[i] = '\0';
     *num_lines = atoi(num_lines_buf);
 
-    // Read the map parameters
     read(fd, empty_char, 1);
     read(fd, obstacle_char, 1);
     read(fd, full_char, 1);
 
-    // Allocate memory for the 2D map array
+    read(fd, &c, 1);
+
     char **map = (char **)malloc(*num_lines * sizeof(char *));
     if (map == NULL) {
         write(2, "Memory allocation failed\n", 25);
         exit(EXIT_FAILURE);
     }
 
-    // Read each line of the map
     for (int i = 0; i < *num_lines; i++) {
         int j = 0;
-        map[i] = (char *)malloc(MAX_LINE_LENGTH + 1);  // +1 for null terminator
+        map[i] = (char *)malloc(MAX_LINE_LENGTH + 1);  
         if (map[i] == NULL) {
             write(2, "Memory allocation failed\n", 25);
             exit(EXIT_FAILURE);
@@ -45,6 +43,8 @@ char **read_map(int fd, int *num_lines, char *empty_char, char *obstacle_char, c
             j++;
         }
         map[i][j] = '\0';
+
+        while (read(fd, &c, 1) > 0 && c != '\n');
     }
 
     return map;
